431d0773209ccfcb3ae760dfcf913ea90f7da692
Anuvinda <anuvindarajesh0@gmail.com>
2025-07-02 11:01:08 +0530

    Added: -Back button to navigate between pages -Automatically add "kg" to the weight -Changed date format in calendar

--- app/src/main/java/com/example/workouttracker/MainActivity.java
+++ app/src/main/java/com/example/workouttracker/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.workouttracker;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -10,29 +12,36 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
     WebView webView;
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         webView = findViewById(R.id.webview);
-        webView.setWebViewClient(new WebViewClient());
-
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
+        webView.setWebViewClient(new WebViewClient());
+
+        // Expose Android interface to JavaScript
+        webView.addJavascriptInterface(new WebAppInterface(), "AndroidApp");
+
         webView.loadUrl("file:///android_asset/index.html");
 
-        // Modern back button handling
         getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
             @Override
             public void handleOnBackPressed() {
-                if (webView.canGoBack()) {
-                    webView.goBack();
-                } else {
-                    finish();
-                }
+                webView.evaluateJavascript("window.dispatchEvent(new CustomEvent('androidBackButton'));", null);
             }
         });
+    }
+
+    // Interface if you ever need to call Android from JS
+    public class WebAppInterface {
+        @JavascriptInterface
+        public void closeApp() {
+            finish(); // optional
+        }
     }
 }
